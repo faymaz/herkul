a9737f125b81b5a6fcc6ffcbaf15dd3a2fee3997
faymaz <fethi.aymaz@gmail.com>
2025-04-27 19:22:52 +0200

    Add Turkish time format localization

--- extension.js
+++ extension.js
@@ -865,7 +865,7 @@ class PrayerTimesIndicator extends PanelMenu.Button {
             totalSeconds: diff.totalSeconds,
             // Türkçe format: "1sa 12dk 30sn"
             //formatted: `${diff.hours}sa ${diff.minutes}dk ${diff.seconds}sn`,
-            formatted: `${diff.hours}sa ${diff.minutes}dk ,
+            formatted: `${diff.hours}sa ${diff.minutes}dk`,
             // İngilizce format: "1h 12m 30s"
             formattedEn: `${diff.hours}h ${diff.minutes}m ${diff.seconds}s`
         };
